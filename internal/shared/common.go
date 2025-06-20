1be6d4b19303be00fd71b519b8a29eed5c3c3c06
Bob Dresner <bob.dresner1@gmail.com>
2025-06-19 20:57:10 -0400

    Adding Assert and CheckError, this will be a quick and dirty Error check to escape the if err != nil then ...

--- internal/shared/common.go
+++ internal/shared/common.go
@@ -1,1 +1,13 @@
 package shared
+
+func Assert(condition bool, msg string) {
+	if !condition {
+		panic("Assertion failed: " + msg)
+	}
+}
+
+func CheckError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
